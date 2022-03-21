842cdcdc7a30703c61b375b61b8a2b00df26220a
Rodolfo Olivieri <rolivier@redhat.com>
2022-03-21 09:18:57 -0300

    testing coverage

--- multi_coverage_demo/main.py
+++ multi_coverage_demo/main.py
@@ -1,3 +1,13 @@
+import platform
+import sys
+
+
 def main() -> int:
+    if platform.system() == "Windows":
+        print("windows code only specific")
+
+    if platform.system() == "Linux":
+        print("linux code only specific")
+
     print("Hello, world!")
     return 0
